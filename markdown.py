f9b359d85f10ba968c79695083dbff316d5da21c
zhenyuxu <zhenyuxu@tesla.com>
2024-11-21 17:05:20 -0800

    added button config

---
+++ markdown.py
@@ -0,0 +1,18 @@
+def markdown():
+    return """
+    <style>
+    button[kind="primary"] {
+        width: 110px!important;
+        height: 50px!important;
+        font-size: 24px!important;
+        background-color: #929492!important;
+        color: #fff!important;
+        border: 1px solid #696868!important;
+        border-radius: 7px!important;
+        cursor: pointer!important;
+    }
+    button[kind="primary"]:hover {
+        background-color: #55a355!important;
+    }
+    </style>
+    """
